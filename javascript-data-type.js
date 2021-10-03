ebc7cd65ccd5c1a066281ffa39f3ac99437042dd
ibrahimkholil <ibrahimkholil122@gmail.com>
2021-10-03 23:20:37 +0600

    learning javascript data type

---
+++ javascript-data-type.js
@@ -0,0 +1,19 @@
+// javascript data type 
+
+/** 
+ * Number 
+ * String 
+ * Null
+ * Object
+ * Boolean
+ * Undefined
+*/
+
+let number = 10; //Number type
+let name = "This is Ibrahim"; // String type
+let isMarried = true ; // Boolean type
+let customer = {firstName:"ibrahim", lastName:"kholil"}
+//console.log(customer.firstName);
+
+
+console.log(typeof(number));
