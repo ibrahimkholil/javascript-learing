5d46d3c16c297f67c7bbd8a69f9913fc2a32e039
ibrahimkholil <ibrahimkholil122@gmail.com>
2021-10-03 23:48:11 +0600

    1 to 100 even odd number programm javascript

---
+++ 1-to-100-odd-number.js
@@ -0,0 +1,15 @@
+
+// Odd number 
+for(i = 1; i <= 100; i += 2){
+  //  console.log('Odd number is :>> ', i); 
+}
+
+// another  even odd 
+for ( i = 0; i <= 100; i++) {
+    //check if the number is even
+   if(i % 2 == 0){
+    console.log('Even  Number is :>> ', i);
+   }else{
+       console.log('Odd number is :>> ', i);
+   }
+}
