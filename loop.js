d750f13c2f2e7a1cbfbfb69886f0ba8b85718e0c
ibrahimkholil <ibrahimkholil122@gmail.com>
2021-11-16 23:52:30 +0600

    js loop

---
+++ loop.js
@@ -0,0 +1,39 @@
+// for loop
+
+let number = 10;
+
+for (let number = 0; number <= 10; number++) {
+   // console.log(number);
+}
+
+//while loop
+//var i = 0; 
+// while (i <= 10 ) {
+//     console.log("Number is " + i);
+//     i++;
+// }
+
+// while with break when i == 10
+
+// while (true) {
+//     console.log(i);
+//     if (10 == i) {
+//         break;
+//     }
+//     i++;
+// }
+
+// i ++ and ++i difference 
+
+var i = 0;
+while (i++ < 5) {
+    console.log(i);
+    
+}
+ console.log("\n");
+
+ var i = 0;
+while (++i < 5) {
+    console.log(i);
+    
+}
