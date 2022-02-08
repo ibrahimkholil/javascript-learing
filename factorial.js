fecbbfeebd59ba238618830be8ea329b20752a64
ibrahimkholil <ibrahimkholil122@gmail.com>
2022-02-08 23:05:16 +0600

    factorial added

---
+++ factorial.js
@@ -0,0 +1,8 @@
+// factorial number 
+var n = 6;
+var  factorial = 1;
+
+for( var i = n; i>1; i--){
+    factorial *=i;
+}
+console.log( " 5 factorial number is: " + factorial);
---
+++ ternary.js
@@ -0,0 +1,9 @@
+var n = 9;
+var result ;
+
+var reminder = n % 2 ;
+
+result = (0 == reminder) ? "even" : "odd";
+
+console.log("this number is:",result);
+
