3b294d66a96957a56c41e8bf9b874dd8757475a0
ibrahimkholil <ibrahimkholil122@gmail.com>
2022-02-09 00:12:02 +0600

    factiorial with recursion

--- factorial.js
+++ factorial.js
@@ -7,8 +7,22 @@ var  factorial = 1;
 // }
 // console.log( " 5 factorial number is:" + factorial);
 
-for(var i = 1; i<= 10 ; i++){
-  factorial*=1;
+// for(var i = 1; i<= 10 ; i++){
+//   factorial*=1;
 
-  console.log("Factorial number",i,"! is",factorial);
+//   console.log("Factorial number",i,"! is",factorial);
+// }
+
+// factorialize by funciton with Recursion
+
+function factorialize(num){
+     if(num < 0){
+         return -1;
+     }else if(num == 0){
+      return 1;
+     }else{
+         return (num * factorialize(num - 1));
+     }
 }
+
+console.log(factorialize(4));
